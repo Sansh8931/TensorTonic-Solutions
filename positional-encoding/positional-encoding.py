47a8610d23b6b47607c70d7d7591349a97ea3064
Sansh8931 <164066331+Sansh8931@users.noreply.github.com>
2026-06-01 12:06:19 +0530

    Add solution for positional-encoding

---
+++ positional-encoding/positional-encoding.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def positional_encoding(seq_len, d_model, base=10000.0):
+
+    positions = np.arange(seq_len)[:, np.newaxis]
+
+    i = np.arange((d_model + 1)//2)[np.newaxis, :]
+
+    div_term = np.power(base, (2 * i) / d_model)
+
+    angles = positions / div_term
+
+    pe = np.zeros((seq_len, d_model), dtype=float)
+
+    pe[:, 0::2] = np.sin(angles)
+
+    pe[:, 1::2] = np.cos(angles[:, :d_model//2])
+
+    return pe
